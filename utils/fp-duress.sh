f07e2e021b7f305d4eb57367decfe72f7e1a1020
debusercccp <temporaryblissstate@gmail.com>
2026-09-05 11:33:15 +0200

    k

--- utils/fp-duress.sh
+++ utils/fp-duress.sh
@@ -3,5 +3,5 @@
 if fprintd-verify -f right-little-finger "$USER" | grep -q "verify-match"; then
     notify-send "Duress trigger" "Esecuzione completata con successo."
     
-    kitty bash -c "awk -F , '{print \$1, \$2, \$3}' /home/noya/ml/datasets/mercurioMurgia.csv; exec bash" &
+    kitty bash -c "awk -F , '{print \$1 , \$2 , \$3}' /home/noya/ml/datasets/mercurioMurgia.csv; exec bash" &
 fi
