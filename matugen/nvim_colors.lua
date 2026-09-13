b4e050d13c640410cf5390d7e1d6ecb2bac6fd75
debusercccp <roccosorix@gmail.com>
2026-09-13 22:43:45 +0200

    LL

---
+++ matugen/nvim_colors.lua
@@ -0,0 +1,6 @@
+return {
+    background = "{{colors.surface.default.hex}}",
+    foreground = "{{colors.on_surface.default.hex}}",
+    primary = "{{colors.primary.default.hex}}",
+    error = "{{colors.error.default.hex}}",
+}
---
+++ nvim/lua/matugen_colors.lua
@@ -0,0 +1,6 @@
+return {
+    background = "#1a1112",
+    foreground = "#f0dedf",
+    primary = "#ffb2ba",
+    error = "#ffb4ab",
+}
